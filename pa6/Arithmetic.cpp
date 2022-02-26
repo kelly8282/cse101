db402f29bc5fb5758d07f03f8dfcd423c824dae5
Kelly Liu <kliu80@ucsc.edu>
2022-02-26 16:43:36 +0000

    sadas

---
+++ pa6/Arithmetic.cpp
@@ -0,0 +1,20 @@
+/*********************************************************************************
+ * Kelly Liu, kliu80
+ * 2022 Winter CSE101 PA5
+ * Arithmetic.cpp
+ * Main function written by me
+ *********************************************************************************/
+
+
+#include<iostream>
+#include<fstream>
+#include<string>
+#include "List.h"
+#include "BigInteger.h"
+using namespace std;
+
+#define MAX_LEN 300
+
+int main(int argc, char * argv[]){
+	cout << "blank" << endl;
+}
